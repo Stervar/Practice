f667f01f6cadf52c2cc223fd418f157c90e49fba
Stervar <150718158+Stervar@users.noreply.github.com>
2024-09-02 21:24:03 +0300

    # Високосный год (2) # Написать функцию is_year_leap, принимающую 1 аргумент — год, и возвращающую True, если год високосный, и False иначе.
    #
    #  def is_year_leap(year):
    #     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
    #         return True
    #     else:
    #         return False
    #         year = int(input("Введите год: "))
    #          result = is_year_leap(year)
    # print(result)
    #

--- practike.py
+++ practike.py
@@ -77,5 +77,18 @@
 #     
    
     
-
+# Високосный год (2)
+# Написать функцию is_year_leap, принимающую 1 аргумент — год, и возвращающую True, если год високосный, и False иначе.
+    
+    
+#     
+#  def is_year_leap(year):
+#     if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+#         return True
+#     else:
+#         return False
+#         year = int(input("Введите год: "))
+#          result = is_year_leap(year)
+# print(result)
+#     
 #
